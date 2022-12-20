b1fb9240691d8c51fd7453bf8bbfb44858a2e6dc
MOHDAHASANKHAN1 <95666818+MOHDAHASANKHAN1@users.noreply.github.com>
2022-12-20 19:09:48 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import styled from "styled-components";
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
 import Controller from "./components/Controller";
 import AlgoDisplay from "./components/AlgoDisplay";
 import Home from "./components/Home";
-import Navbarh from './components/Navbarh';
+import Navbarh from "./components/Navbarh";
 
 const Container = styled.div`
   margin: 0 10px;
@@ -17,12 +17,32 @@ const Container = styled.div`
 
 export default function App() {
   return (
+    <BrowserRouter>
+      <Navbarh />
+      <Routes>
+        <Route
+          exact
+          path="/"
+          element={
+            <>
+              <NavBar />
+              <Controller />
+              <AlgoDisplay />
+            </>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+/*export default function App() {
+  return (
 <BrowserRouter>
  <Navbarh/>
   <Routes>
 	<Route exact path="/" element={<><Home/></>}/>
-	<Route exact path="/visualize" element={<><NavBar/><Controller/><AlgoDisplay/></>}/>
+	<Route exact path="/" element={<><NavBar/><Controller/><AlgoDisplay/></>}/>
   </Routes>
 </BrowserRouter>
   );
-}
+}*/
