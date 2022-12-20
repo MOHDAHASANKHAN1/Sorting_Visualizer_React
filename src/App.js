f6b6585bfe938c7362528845cb034ff20eb88d36
MOHDAHASANKHAN1 <95666818+MOHDAHASANKHAN1@users.noreply.github.com>
2022-12-20 19:19:00 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -25,7 +25,6 @@ export default function App() {
           path="/"
           element={
             <>
-              <NavBar />
               <Controller />
               <AlgoDisplay />
             </>
